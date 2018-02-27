8d642ef7006a5695f78145ba034d916669439495
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-02-27 16:11:51 -0600

    getting started

---
+++ foodie-list.js
@@ -0,0 +1,6 @@
+"use strict";
+//console.log("it is working"); //making sure we are linked:)
+
+//making a XHR to get ratting's
+const XHR = new XMLHttpRequest(); //asigning a constant variable to the XHR
+
